d2cf37f3919eb89f9377ba2b8412c32e2ee17419
Angad Kaflay <36495456+akaflay@users.noreply.github.com>
2019-02-15 22:53:10 -0800

    Update form.js

--- form.js
+++ form.js
@@ -4,7 +4,7 @@
         return new Form.init(options);
     };
     let parentElement;
-    let arrangement=[];
+    let arrangement = [];
     let formElem;
 
     Form.init = function (options) {
@@ -12,29 +12,161 @@
         self.validate(options);
         return this;
     };
+
+    let createEachTable = (self, table) => {
+        let div = document.createElement("div");
+        div.classList.add('each-form-section');
+        let label = document.createElement("div");
+        label.innerText = table.label || 'Table Label';
+        label.className = table.labelClassName;
+        div.appendChild(label);
+        let htmlTable = document.createElement('table');
+        htmlTable.className = table.className;
+        let tableHeaderRow = document.createElement('tr');
+        table.header.forEach(function (item) {
+            let tableHeaderRowTD = document.createElement('th');
+            tableHeaderRowTD.innerText = item;
+            tableHeaderRow.appendChild(tableHeaderRowTD);
+        });
+        htmlTable.appendChild(tableHeaderRow);
+
+
+        table.data.forEach(function (dataList) {
+            let tableBodyRow = document.createElement('tr');
+            table.header.forEach(function (item) {
+                let tableBodyRowTD = document.createElement('td');
+                tableBodyRowTD.innerText = dataList[item];
+                tableBodyRow.appendChild(tableBodyRowTD);
+
+            });
+            tableBodyRow.addEventListener('click', table.onRowClick.bind(null, dataList, tableBodyRow));
+            tableBodyRow.className = table.rowClassName;
+            htmlTable.appendChild(tableBodyRow);
+
+
+        });
+
+
+        div.appendChild(htmlTable);
+        self.arrangeElements(table.index, div);
+
+    };
+
+    let createEachButton = (self, button) => {
+        let div = document.createElement("div");
+        let btn = document.createElement('button');
+        btn.innerText = button.value || 'Button';
+        btn.className = button.className;
+        btn.addEventListener('click', button.onSubmit);
+        div.appendChild(btn);
+        self.arrangeElements(button.index, div);
+
+    };
+
+    let createEachCheckBoxOrRadioButton = (inputItemDiv,input,item) => {
+        let inputItem = document.createElement("input");
+        inputItem.name = input.name;
+        inputItem.type = input.type;
+        inputItem.value = item;
+        inputItem.id = item;
+        let label = document.createElement('label');
+        label.className = input.className;
+        label.for = item;
+        label.innerText = item;
+        inputItemDiv.appendChild(label).appendChild(inputItem);
+
+
+
+    };
+
+    let createEachSelectOption=(inputItem,item)=>{
+        let itm = document.createElement("option");
+        itm.value = item.value;
+        itm.innerText = item.label;
+        inputItem.appendChild(itm);
+
+    };
+
+    let createEachItem=(inputItemDiv,input)=>{
+        let inputItem = document.createElement("input");
+        inputItem.className = input.className;
+        inputItem.name = input.name;
+        inputItemDiv.appendChild(inputItem);
+
+    };
+
+    let createEachInput = (self, input) => {
+        //Create outer div
+        let div = document.createElement("div");
+        div.classList.add("each-form-section");
+
+        //Create Label Div
+        let label = document.createElement("div");
+        label.innerText = input.label;
+        label.classList.add('float-left');
+        label.classList.add(input.lableClassName);
+
+        //Create Input div
+        let inputItemDiv = document.createElement("div");
+        inputItemDiv.class = `float-left`;
+
+        if (input.type === 'text') {
+            createEachItem(inputItemDiv,input);
+
+        }
+
+        if (input.type === 'select') {
+            if (!input.items || !Array.isArray(input.items)) throw "Please provide proper options for select";
+            let inputItem = document.createElement("select");
+            inputItem.className = input.className;
+            inputItem.name = input.name;
+            if (Array.isArray(input.items)) {
+                input.items.forEach(function (item) {
+                    createEachSelectOption(inputItem,item);
+
+                });
+            }
+            inputItemDiv.appendChild(inputItem);
+        }
+
+        if (input.type === 'checkbox' || input.type === 'radio') {
+            if (!input.items || !Array.isArray(input.items)) throw `Please provide proper options for ${input.type}`;
+            input.items.forEach(function (item) {
+                createEachCheckBoxOrRadioButton(inputItemDiv,input,item);
+            });
+        }
+        //Append label and item div
+        div.appendChild(label);
+        div.appendChild(inputItemDiv);
+
+        //Add outer div to the arrangment array
+        self.arrangeElements(input.index, div);
+    };
+
+
     Form.prototype = {
 
-        createForm:function(){
+        createForm: function () {
             formElem = document.createElement("form");
             return this;
         },
 
-        createHTML:function(){
-            if(!formElem) this.createForm();
+        createHTML: function () {
+            if (!formElem) this.createForm();
 
-            if(arrangement.length>0){
-                arrangement.forEach((item=>formElem.appendChild(item)));
+            if (arrangement.length > 0) {
+                arrangement.forEach((item => formElem.appendChild(item)));
             }
             parentElement.appendChild(formElem);
 
         },
 
-        arrangeElements:function(index, element){
+        arrangeElements: function (index, element) {
 
-            if(arrangement[index]){
+            if (arrangement[index]) {
                 arrangement.push(element);
-            }else{
-                arrangement[index]=element;
+            } else {
+                arrangement[index] = element;
             }
         },
 
@@ -52,151 +184,44 @@
         },
 
         createInput: function (inputs) {
-            let self=this;
+            let self = this;
 
             //For each input options loop and create inner items
             if (inputs && Array.isArray(inputs)) {
 
                 inputs.forEach(function (val) {
-
-
-                    //Create outer div
-                    let div = document.createElement("div");
-                    div.classList.add("each-form-section");
-
-                    //Create Label Div
-                    let label = document.createElement("div");
-                    label.innerText = val.label;
-                    label.classList.add('float-left');
-                    label.classList.add(val.lableClassName);
-
-                    //Create Input div
-                    let inputItemDiv = document.createElement("div");
-                    inputItemDiv.class = `float-left`;
-                    if (val.type === 'text') {
-                        let inputItem = document.createElement("input");
-                        inputItem.className = val.className;
-                        inputItem.name = val.name;
-                        inputItemDiv.appendChild(inputItem);
-
-                    }
-                    if (val.type === 'select') {
-                        let inputItem = document.createElement("select");
-                        inputItem.className = val.className;
-                        inputItem.name = val.name;
-                        if (Array.isArray(val.items)) {
-                            val.items.forEach(function (item) {
-                                let itm = document.createElement("option");
-                                itm.value = item.value;
-                                itm.innerText = item.label;
-                                inputItem.appendChild(itm);
-                            });
-                        }
-
-                        inputItemDiv.appendChild(inputItem);
-
-                    }
-
-                    if (val.type === 'checkbox' || val.type === 'radio') {
-                        if (Array.isArray(val.items)) {
-                            val.items.forEach(function (item) {
-
-                                let inputItem = document.createElement("input");
-                                inputItem.name = val.name;
-                                inputItem.type = val.type;
-                                inputItem.value = item;
-                                inputItem.id = item;
-                                let label = document.createElement('label');
-                                label.className=val.className;
-                                label.for = item;
-                                label.innerText = item;
-                                inputItemDiv.appendChild(label).appendChild(inputItem);
-                            });
-
-                        }
-                    }
-                    //Append label and item div
-                    div.appendChild(label);
-                    div.appendChild(inputItemDiv);
-
-                    //Add outer div to the arrangment array
-                    self.arrangeElements(val.index,div);
+                    createEachInput(self, val);
                 });
 
             }
 
-
-
-
             return self;
 
         },
-        createTable:function(tables){
-            let self=this;
+        createTable: function (tables) {
+            let self = this;
             if (tables && Array.isArray(tables)) {
 
                 tables.forEach(function (tableItem) {
-                    let div = document.createElement("div");
-                    div.classList.add('each-form-section');
-                    let label = document.createElement("div");
-                    label.innerText = tableItem.label;
-                    label.className = tableItem.labelClassName;
-                    div.appendChild(label);
-                    let htmlTable = document.createElement('table');
-                    htmlTable.className = tableItem.className;
-                    let tableHeaderRow = document.createElement('tr');
-                    tableItem.header.forEach(function (item) {
-                        let tableHeaderRowTD = document.createElement('th');
-                        tableHeaderRowTD.innerText = item;
-                        tableHeaderRow.appendChild(tableHeaderRowTD);
-                    });
-                    htmlTable.appendChild(tableHeaderRow);
+                    createEachTable(self, tableItem);
 
-
-                    tableItem.data.forEach(function (dataList) {
-                        let tableBodyRow = document.createElement('tr');
-                        tableItem.header.forEach(function (item) {
-                            let tableBodyRowTD = document.createElement('td');
-                            tableBodyRowTD.innerText = dataList[item];
-                            tableBodyRow.appendChild(tableBodyRowTD);
-
-                        });
-                        tableBodyRow.addEventListener('click', tableItem.onRowClick.bind(null, dataList, tableBodyRow));
-                        tableBodyRow.className = tableItem.rowClassName;
-                        htmlTable.appendChild(tableBodyRow);
-
-
-                    });
-
-
-                    div.appendChild(htmlTable);
-                    self.arrangeElements(tableItem.index, div);
                 });
             }
             return self;
 
         },
 
-        createButton:function(buttons){
-            let self=this;
+        createButton: function (buttons) {
+            let self = this;
             if (buttons && Array.isArray(buttons)) {
 
                 buttons.forEach(function (item) {
-                    let div = document.createElement("div");
-                    let button = document.createElement('button');
-                    button.innerText = item.value;
-                    button.className = item.className;
-                    button.addEventListener('click' , item.onSubmit);
-                    div.appendChild(button);
-                    self.arrangeElements(item.index, div);
+                    createEachButton(self, item);
                 });
             }
             return self;
 
         }
-
-
-
 
     };
 
